27f7464b61f2a9c7e553d18d2628f99851c5553f
Alex <skyriver.f1@gmail.com>
2022-08-22 13:34:45 +0500

    added article content & next article & recom article & more

--- marmelad/_blocks/article-content/article-content.js
+++ marmelad/_blocks/article-content/article-content.js
@@ -2,6 +2,15 @@ $(".article-content__video").click(function () {
   let $this = $(this);
   let videoSrc = $this.data('video');
 
+  $this.addClass('_is-play')
+
   $this.find('iframe').attr('src', videoSrc);
   $this.find('img').fadeOut();
 });
+
+$('.article-content__find-answer h3').on('click', function () {
+  $(this).toggleClass('_is-active')
+  $(this)
+        .closest('.article-content__find-answer')
+        .find('.article-content__find-answer-inner').stop().slideToggle();
+}) 
--- marmelad/_blocks/articles-day/articles-day.js
+++ marmelad/_blocks/articles-day/articles-day.js
@@ -1,7 +1,9 @@
 function titleArticlesAnimate() {
 
-  const getOffsetArticlesDayBlock = $('.articles-day').offset().top - 300;
-  const getOffsetOtherBlock = $('.other-articles').offset().top - 200;
+  const getOffsetArticlesDayBlock = $('.articles-day').length ? $('.articles-day').offset().top - 300 : null;
+  const getOffsetOtherBlock = $('.other-articles').length ? $('.other-articles').offset().top - 1000 : null;
+  const getOffsetRecommendedBlock = $('.recommended-article').length ? $('.recommended-article').offset().top - 300 : null;
+  const getOffsetNextArticledBlock = $('.next-article').length ? $('.next-article').offset().top - 300 : null;
 
   $(window).on('scroll', function () {
     if ($(this).scrollTop() >= getOffsetArticlesDayBlock) {
@@ -14,6 +16,16 @@ function titleArticlesAnimate() {
       $('.other-articles').addClass('_is-visible');
     } else {
       $('.other-articles').removeClass('_is-visible');
+    }
+    if ($(this).scrollTop() >= getOffsetRecommendedBlock) {
+      $('.recommended-article').addClass('_is-visible');
+    } else {
+      $('.recommended-article').removeClass('_is-visible');
+    }
+    if ($(this).scrollTop() >= getOffsetNextArticledBlock) {
+      $('.next-article').addClass('_is-visible');
+    } else {
+      $('.next-article').removeClass('_is-visible');
     }
   });
 
