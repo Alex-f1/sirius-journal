919a7714675ce4f064fa620bfdc3f3b33351b3d7
Alex <skyriver.f1@gmail.com>
2022-08-26 20:06:32 +0500

    added about project page & more

--- marmelad/_blocks/articles-day/articles-day.js
+++ marmelad/_blocks/articles-day/articles-day.js
@@ -5,11 +5,13 @@ function titleArticlesAnimate() {
   const getOffsetRecommendedBlock = $('.recommended-article').length ? $('.recommended-article').offset().top - 300 : null;
   const getOffsetNextArticledBlock = $('.next-article').length ? $('.next-article').offset().top - 300 : null;
   const getOffsetOtherPodcastsBlock = $('.other-podcasts').length ? $('.other-podcasts').offset().top - 300 : null;
+  const getOffsetAboutProjectBlock = $('.about-project').length ? $('.about-project').offset().top - 300 : null;
+  const getOffsetEditorialBlock = $('.editorial').length ? $('.editorial').offset().top - 300 : null;
+  const getOffsetOurContactsBlock = $('.our-contacts').length ? $('.our-contacts').offset().top - 300 : null;
 
   $(window).on('scroll', function () {
     if ($(this).scrollTop() >= getOffsetArticlesDayBlock) {
       $('.articles-day').addClass('_is-visible');
-
     } else {
       $('.articles-day').removeClass('_is-visible');
     }
@@ -32,6 +34,21 @@ function titleArticlesAnimate() {
       $('.other-podcasts').addClass('_is-visible');
     } else {
       $('.other-podcasts').removeClass('_is-visible');
+    }
+    if ($(this).scrollTop() >= getOffsetAboutProjectBlock) {
+      $('.about-project').addClass('_is-visible');
+    } else {
+      $('.about-project').removeClass('_is-visible');
+    }
+    if ($(this).scrollTop() >= getOffsetEditorialBlock) {
+      $('.editorial').addClass('_is-visible');
+    } else {
+      $('.editorial').removeClass('_is-visible');
+    }
+    if ($(this).scrollTop() >= getOffsetOurContactsBlock) {
+      $('.our-contacts').addClass('_is-visible');
+    } else {
+      $('.our-contacts').removeClass('_is-visible');
     }
   });
 
